c98ff5e4ab960de03136cc74babebac50ca8d763
AnilSai28 <46150420+AnilSai28@users.noreply.github.com>
2018-12-31 14:41:26 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_oop
+{
+    class Class1
+    {
+        public int orderid;
+        public string customername;
+        public string itemname;
+        public int itemquantity;
+        public int itemprice;
+        public int getordervalue()
+        {
+        return itemquantity* itemprice;
+    
+    }
+}
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter order id : ");
+            int oredrid = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter customer name : ");
+            string customername = Console.ReadLine();
+
+            Console.WriteLine("enter item name : ");
+            string itemname = Console.ReadLine();
+
+            Console.WriteLine("enter item quantity : ");
+            int itemquantity = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter item price : ");
+            int itemprice = Convert.ToInt32(Console.ReadLine());
+
+            oop obj = new oop();
+            obj.itemquantity = itemquantity;
+            obj.itemprice = itemprice;
+            int result = obj.getordervalue();
+            Console.WriteLine("order value :" + result);
+
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+    }
+}
